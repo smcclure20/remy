16933bba9bddc3e718a5b31f0d30a9320129a090
Sarah McClure <sarahmcclure97@gmail.com>
2023-12-04 16:20:21 -0800

    fixing issue where link.hh had packets added to link before int headers sent

--- src/link.hh
+++ src/link.hh
@@ -79,9 +79,9 @@ public:
 
   void accept( Packet & p, const double & tickno ) noexcept {
     if ( _pending_packet.empty() ) {
-      _pending_packet.accept( p, tickno );
       add_packet_to_counter(tickno);
       set_int_fields(p);
+      _pending_packet.accept( p, tickno );
     } else {
       if ( _limit and _buffer.size() < _limit ) {
         
